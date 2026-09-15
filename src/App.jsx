e4d19808378af505ac7ba4f12af011b3ee1c8ebd
visuraransika <visuraliayanagee@gmail.com>
2026-09-15 17:16:44 +0530

    day 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 import './App.css';
+import Test from './components/test';
 
 function App() {
 
 return (
-    
-      <div className="border w-600 h-600  bg-yellow-500 relative">
-      <div className="w-500 h-500 bg-blue-700 flex flex-col items-center justify-center">
-      <div className="w-100 h-100 bg-green-500"></div>
-      <div className="w-100 h-100 bg-red-700 fixed left-[150] top-[150]" ></div>
-      <div className="w-100 h-100 bg-purple-800"></div>
-      <div className="w-100 h-100 bg-pink-800 absolute left-[50] right-[50]"></div>
+    <div>
 
-      
+<Test/>
+        
     </div>
-    </div>
+    
+
+
   );
 }
 
---
+++ src/components/test.jsx
@@ -0,0 +1,14 @@
+export default function Test() {
+
+    let count=0;
+    
+    return(
+        <div className="w-full h-[600] bg-red-500">
+
+            <div className="w-[300] h-[300] bg-blue-700">
+            </div>
+
+        </div>
+
+    )
+}
